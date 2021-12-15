719d73a80d38e8b4024cd23399f22675513645dc
casa92 <casartelli92@gmail.com>
2021-12-15 17:28:28 +0100

    milestone-5

---
+++ milestone-5/js/script.js
@@ -0,0 +1,187 @@
+
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            userFilterText: '',
+            currentActiveContact: 0,
+            currentActiveMessage: null,
+            newMessage: '',
+            thisContactImg : 'img/avatar',
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Luisa',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+            ]
+        },
+     
+        methods: {
+            //al click mostra il contatto selezionato e se il messaggio è attivo lo resetta
+            showThisContact: function(index) {
+                this.currentActiveContact = index;
+                this.currentActiveMessage = null;
+            },
+            //al click invia messaggio elimando spazi con trim, pushandolo nell'array tra i messaggi dell'elemento attivo.
+            //dopo 1 secondo viene inviato dal pc un messaggio con classe received pushandolo a suo volta nell'array dello stesso elemento
+            sendNewMessage: function() {
+                const newMessageTrimmed = this.newMessage.trim();
+                if (newMessageTrimmed.length > 2) {
+                    this.contacts[this.currentActiveContact].messages.push({
+                        //data e ora del momento
+                        date: dayjs().format("DD/MM/YYYY HH:mm:ss"),
+                        text: newMessageTrimmed,
+                        status: 'sent'
+                    });
+                    this.newMessage = '';
+
+                    setTimeout(() => {
+                        this.contacts[this.currentActiveContact].messages.push({
+                            date: dayjs().format("DD/MM/YYYY HH:mm:ss"),
+                            text: 'ok',
+                            status: 'received'
+                        })
+                    }, 1000);
+                    
+                };
+               
+            },
+            //filtro che non considera spazi e maiuscole
+            //se la sequenza di lettere è presente tra i nomi del mio array allora l'elemento sarà visibile, altrimenti no
+            searchFilterText: function() {
+                const newSearchFilterText = this.userFilterText.trim();
+                this.contacts.forEach((element) => {
+                    if( element.name.toLowerCase().includes( newSearchFilterText.toLowerCase())) {
+                        element.visible = true;
+                    } else {
+                        element.visible = false;
+                    }
+                });
+            },
+            //al click sull'elemento selezionato il mesaggio diventa attivo
+            thisMessageIsActive: function(index) {
+                if(index === this.currentActiveMessage) {
+                    this.currentActiveMessage = null;
+                } else {
+                    this.currentActiveMessage = index;
+                }
+            },
+            //se lascio il messagio attivato si chiude in automatico
+            MouseLeaveMessageActive: function(index) {
+                this.currentActiveMessage = null;
+            },
+            //al click elimino l'elemento corrente dall'array di messaggi
+            deleteMessage: function(index) {
+                this.contacts[this.currentActiveContact].messages.splice(index, 1);
+            },
+            //funzione che estrae la data dell'ultimo messaggio presente nel array dei contatti
+            singleContactLastMessageDate: function(contact) {
+                const contactMessages = contact.messages;
+                return contactMessages[contactMessages.length - 1].date;
+            },
+            //funzione che estrae l'ultimo messagio dell'array e va a mostrare l'anteprima dei primi 25 caratteri, oltre vengono inseriti '...'
+            singleContactLastMessageText: function(contact) {
+                const contactMessages = contact.messages;
+                const lastMessage = contactMessages[contactMessages.length - 1].text;
+                let cutMessage = lastMessage.slice(0, 25);
+                if(cutMessage.length > 24) {
+                    cutMessage  += '...';
+                }
+                return cutMessage;
+            }
+            
+        },
+        //all'invio di ogni nuovo messaggio la pagina si scrolla verso il basso in automatico
+        updated() {
+            const container = this.$el.querySelector("#chat-container");
+            container.scrollTop = container.scrollHeight;
+        },
+
+        //al caricamento della pagina se la conversazione è più lunga dello schermo e viene mostrata scroll, la pagina verrà caricata mostrando l'ultimo messaggio inviato
+        mounted() {
+            const container = this.$el.querySelector("#chat-container");
+            container.scrollTop = container.scrollHeight;
+        }
+    }
+)
